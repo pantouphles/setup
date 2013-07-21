9fdefba94abb50b5fbe16616e0d7eb84f60df1db
Pantouphles <pantouphles@gmail.com>
2013-07-21 16:06:17 +0000

    Updated git config

--- setup.sh
+++ setup.sh
@@ -41,3 +41,6 @@ ln -sb dotfiles/.bashrc .
 ln -sb dotfiles/.bashrc_custom .
 ln -sf dotfiles/.emacs.d .
 
+git config --global user.name "Pantouphles"
+git config --global user.email "pantouphles@gmail.com"
+
