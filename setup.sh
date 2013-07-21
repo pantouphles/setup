3e22afbb5804d6434ed83939b4de6f441ff2c1bd
Pantouphles <pantouphles@gmail.com>
2013-07-21 17:06:31 +0000

    Updated dotfiles url to use ssh

--- setup.sh
+++ setup.sh
@@ -34,7 +34,7 @@ fi
 if [ -d .emacs.d/ ]; then
     mv .emacs.d .emacs.d~
 fi
-git clone https://github.com/pantouphles/dotfiles.git
+git clone git@github.com:pantouphles/dotfiles.git
 ln -sb dotfiles/.screenrc .
 ln -sb dotfiles/.bash_profile .
 ln -sb dotfiles/.bashrc .
